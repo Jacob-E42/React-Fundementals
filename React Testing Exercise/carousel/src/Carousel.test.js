5e7ac7f3cb0ec62c3760eed3b4b4b601c27bcfd2
Jacob Eiferman <jacobeiferman@gmail.com>
2023-02-22 12:35:16 -0500

    fixed left arrow going forward bug

--- React Testing Exercise/carousel/src/Carousel.test.js
+++ React Testing Exercise/carousel/src/Carousel.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+it("renders without crashing", function () {
+	render(<Carousel />);
+});
+
+it("mathces snapshot", function () {
+	const { asFragment } = render(<Carousel />);
+	expect(asFragment()).toMatchSnapshot();
+});
+
 it("works when you click on the right arrow", function () {
 	const { queryByTestId, queryByAltText } = render(<Carousel />);
 
@@ -18,11 +27,18 @@ it("works when you click on the right arrow", function () {
 	expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
 
-it("renders without crashing", function () {
-	render(<Carousel />);
-});
+it("works when you click on the left arrow", function () {
+	const { queryByTestId, queryByAltText } = render(<Carousel />);
 
-it("mathces snapshot", function () {
-	const { asFragment } = render(<Carousel />);
-	expect(asFragment()).toMatchSnapshot();
+	// expect the first image to show, but not the second
+	expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+	expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+	// move forward in the carousel
+	const leftArrow = queryByTestId("left-arrow");
+	fireEvent.click(leftArrow);
+
+	// expect the third image to show, but not the first
+	expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+	expect(queryByAltText("Photo by Josh Post on Unsplash")).toBeInTheDocument();
 });
