cb58d7d0230a6b69f1dd49b386870bba09dfac9a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-03-02 11:39:36 -0500

    React forms exercises, initial commit

---
+++ React- Forms/color-box-maker/src/BoxList.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import Box from "./Box.js";
+import NewBoxForm from "./NewBoxForm.js";
+
+const BoxList = () => {};
+
+export default BoxList;
---
+++ React- Forms/color-box-maker/src/NewBoxForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewBoxForm = () => {
+	return <form></form>;
+};
+
+export default NewBoxForm;
