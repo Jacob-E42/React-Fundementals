cfb7d08604428faac3ce2b80f3567b50925d461f
Jacob Eiferman <jacobeiferman@gmail.com>
2023-03-09 12:21:35 -0500

    initial commit for Part 2

---
+++ React Effects - Deck of Cards/Part 2/src/Card.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Card = ({ image }) => {
+	return (
+		<img
+			alt="individual card"
+			src={image}
+		/>
+	);
+};
+
+export default Card;
---
+++ React Effects - Deck of Cards/Part 2/src/CardDeck.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, useRef } from "react";
+import Card from "./Card";
+import axios from "axios";
+
+const CardDeck = () => {
+	const deckId = useRef();
+	const [cardsRemaining, setCardsRemaining] = useState(52);
+	const [currentCard, setCurrentCard] = useState(null);
+
+	useEffect(() => {
+		async function setUpCardDeck() {
+			const { data } = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/");
+			deckId.current = data.deck_id;
+		}
+		setUpCardDeck();
+	}, []);
+
+	// useEffect(() => {
+	// 	if (cardsRemaining <= 0) {
+	// 		displayCard = () => {
+
+	// 		};
+	// 	}
+	// }, [cardsRemaining]);
+
+	let displayCard = () => {
+		if (cardsRemaining >= 1) {
+			async function drawCard() {
+				const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId.current}/draw`);
+				const { cards } = response.data;
+				setCardsRemaining(() => cardsRemaining - 1);
+				setCurrentCard(() => {
+					return <Card image={cards[0].image} />;
+				});
+			}
+			drawCard();
+		} else alert("Error: no cards remaining!");
+	};
+
+	return (
+		<div>
+			<button onClick={displayCard}>Give me a Card!</button>
+			<p>Number of Cards Remaining: {cardsRemaining}</p>
+			<div>{currentCard}</div>
+		</div>
+	);
+};
+
+export default CardDeck;
