6b334ecf06c43f24900ff2dde3921c834ff25d9a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-03-13 19:36:11 -0400

    created useFlip custom hook

--- react-cards-pokemon/src/PlayingCard.js
+++ react-cards-pokemon/src/PlayingCard.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import backOfCard from "./back.png";
-import "./PlayingCard.css"
+import { useFlip } from "./hooks";
+import "./PlayingCard.css";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
-  return (
-    <img
-      src={isFacingUp ? front : back}
-      alt="playing card"
-      onClick={flipCard}
-      className="PlayingCard Card"
-    />
-  );
+	const [isFacingUp, flip] = useFlip();
+
+	return (
+		<img
+			src={isFacingUp ? front : back}
+			alt="playing card"
+			onClick={flip}
+			className="PlayingCard Card"
+		/>
+	);
 }
 
 export default PlayingCard;
